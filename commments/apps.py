54f7d810d4423509d292a16ea706275be2c5ae64
Ozodbek <ahmadovozodbek80@gmail.com>
2025-06-04 23:05:58 +0500

    Project again started../

--- commments/apps.py
+++ commments/apps.py
@@ -1,9 +1,6 @@
 from django.apps import AppConfig
 
 
-class SearchConfig(AppConfig):
+class CommmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'search'
-
-    def ready(self):
-        pass
+    name = 'commments'
